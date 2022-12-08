1af46bb1fbae96ba0bacdc0683acf0dec9a7d2df
nyaks1 <nyakallomasiu@gmai.com>
2022-12-08 04:49:59 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
